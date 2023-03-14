9cb45b17af7a3059d39f48e73a1193b4d8e5d31f
Sayat Arakelyan <sayat.araqelyan9725@gmail.com>
2023-03-14 23:39:58 +0300

    add instruction

--- src/layout/instructions/Instructions.js
+++ src/layout/instructions/Instructions.js
@@ -4,8 +4,8 @@ import "./style.css"
 function Instructions() {
     return (
         <aside className="aside">
-         <h2>press the "add card" button to add the new card Use the "sort card" button to sort the cards press x to
-             delete item</h2>
+         <p>press the "add card" button to add the new card Use the "sort card" button to sort the cards press x to
+             delete item</p>
 
 
         </aside>
