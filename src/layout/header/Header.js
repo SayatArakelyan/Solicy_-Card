6a8719ba5c4e5e8e26e653b558480e0b2c7610d1
Sayat Arakelyan <sayat.araqelyan9725@gmail.com>
2023-03-15 09:36:44 +0300

    add scrollbar

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,10 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect,useCallback} from 'react';
 import {addToLocalStorage, getLocalStorage} from "../../helpers/storage";
 import Main from "../main/Main";
 import "./style.css"
 
 function Header() {
     const [randomNumber, setRandomNumber] = useState(getLocalStorage('data') || [])
+
+    const ref = useRef()
+    // The scroll listener
+    const handleScroll = useCallback(() => {
+        console.log("scrolling")
+    }, [])
+
+    // Attach the scroll listener to the div
+    useEffect(() => {
+        const div = ref.current
+        div.addEventListener("scroll", handleScroll)
+    }, [handleScroll])
 
     const handleSort = () => {
         setRandomNumber((prevState) => {
@@ -39,10 +51,11 @@ function Header() {
 
     return (
         <header className="header">
+            <div className="scrollableContainer" ref={ref}>
             <button onClick={handleAdd} className="onAdd">Add Card</button>
             <button onClick={handleSort} className="onSort">Sort Cards</button>
             <Main randomNumbers={randomNumber} onDelete={handleDelete} />
-
+            </div>
 
         </header>
     );
--- src/layout/instructions/Instructions.js
+++ src/layout/instructions/Instructions.js
@@ -5,7 +5,7 @@ function Instructions() {
     return (
         <aside className="aside">
          <p>press the "add card" button to add the new card Use the "sort card" button to sort the cards press x to
-             delete item</p>
+             delete items</p>
 
 
         </aside>
